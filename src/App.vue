48409457ac5d136ef875308e5b4631f1e29c38f2
kromate <akpananthony33@gmail.com>
2022-03-31 12:19:48 +0100

    feat: update

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,5 @@ import HelloWorld from './components/HelloWorld.vue'
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
   ],
   mode:'jit',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#20083BBF"
+      }
+    },
   },
   plugins: [],
 }

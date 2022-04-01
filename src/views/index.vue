5c340cc5a4f83365620fd7ad5cbff52b08f3aad4
kromate <akpananthony33@gmail.com>
2022-04-01 03:47:33 +0100

    feat: update

--- src/views/index.vue
+++ src/views/index.vue
@@ -6,19 +6,18 @@
 				class="container max-w-lg px-4 py-32 mx-auto text-left md:max-w-none md:text-center"
 			>
 				<h1
-					class="text-5xl font-extrabold leading-10 tracking-tight text-left text-gray-900 md:text-center sm:leading-none md:text-6xl lg:text-7xl"
+					class="text-5xl font-extrabold leading-10 tracking-normal text-left text-gray-900 md:text-center sm:leading-none md:text-6xl lg:text-7xl"
 				>
-					<span class="inline md:block">Start Crafting Your</span>
+					<span class="inline md:block">Spend more time Building</span>
 					<span
-						class="relative mt-2 text-transparent bg-clip-text bg-gradient-to-br from-indigo-600 to-indigo-500 md:inline-block"
-					>Next Great Idea</span
+						class="relative mt-2 text-transparent bg-clip-text gradient md:inline-block text-primary"
+					>less time Coding</span
 					>
 				</h1>
 				<div
-					class="mx-auto mt-5 text-gray-500 md:mt-12 md:max-w-lg md:text-center lg:text-lg"
+					class="mx-auto mt-5 text-gray-500 md:mt-12 md:max-w-4xl md:text-center lg:text-lg"
 				>
-					Simplifying the creation of landing pages, blog pages, application
-					pages and so much more!
+					An Open Source Project that is focused on helping you spend less time on coding UI designs and more time building the actual application by providing already made components and pages
 				</div>
 				<div class="flex flex-col items-center mt-12 text-center">
 					<span class="relative inline-flex w-full md:w-auto">
@@ -40,3 +39,30 @@
 //@ts-ignore
 import NavBar from '../components/NavBar.vue'
 </script>
+
+
+<style scoped>
+.gradient {
+  background: #8A2387;
+   background: linear-gradient(to right, #F27121 20%, #E94057 40%, #E94057 60%, 8A2387 80%);
+  background-size: 200% auto;
+  background-clip: text;
+
+    animation: shine 1s linear infinite;
+  }
+
+@keyframes hue {
+  from {
+    -webkit-filter: hue-rotate(0deg);
+  }
+  to {
+    -webkit-filter: hue-rotate(-360deg);
+  }
+}
+
+
+</style>
+
+
+
+//   background: linear-gradient(to right, #F27121, #E94057, #8A2387); 

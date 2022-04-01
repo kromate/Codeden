28f813336222c59255f8fc40c65be1eab367cbab
kromate <akpananthony33@gmail.com>
2022-04-01 19:00:09 +0100

    --- Auto-Git Commit ---

--- src/composables/useFileSystem.ts
+++ src/composables/useFileSystem.ts
@@ -1,6 +1,6 @@
 export const readBlocks = async () => {
 
-    let result = []
+    let result: string[] = []
 
     const requireComponent = import.meta.glob('../../blocks/*/index.vue')
     const BlockArr = Object.keys(requireComponent)

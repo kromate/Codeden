cb0d605be13e0347e0a6f2d40a2ddff6d63c8f2b
kromate <akpananthony33@gmail.com>
2022-04-01 16:34:30 +0100

    feat: update

--- src/components/stage/MainView.vue
+++ src/components/stage/MainView.vue
@@ -1,5 +1,6 @@
 <template>
-  
+	<div class="w-full card ">
+	</div>
 </template>
 
 <script lang="ts" setup>
---
+++ src/components/stage/SIdeMenu.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="w-40 h-full card flex flex-col items-start py-5 px-5 gap-2">
+		<span class="self-start inline-block text-slate-500 px-2 py-1  rounded-md cursor-pointer font-bold hover:bg-slate-200 w-full" v-for="n in 12" :key="n">Header</span>
+	</div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style  scoped>
+
+</style>

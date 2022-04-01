bd7e3392b1222910925d3cc8757aee5a570f5537
kromate <akpananthony33@gmail.com>
2022-04-01 16:05:36 +0100

    --- Auto-Git Commit ---

--- src/views/stage/index.vue
+++ src/views/stage/index.vue
@@ -1,7 +1,7 @@
 <template>
-	<div>
-		Hello World
-	</div>
+	<nav class="bg-primary">
+		<span class="p-1 text-xl font-black leading-none text-gray-900"><span>CodeDen</span><span class="text-indigo-600">.</span></span>
+	</nav>
 </template>
 
 <script lang="ts" setup>

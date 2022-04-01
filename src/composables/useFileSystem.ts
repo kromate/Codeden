69c7a246cf20344e3e626d6d990e4529c6a14170
kromate <akpananthony33@gmail.com>
2022-04-01 18:59:04 +0100

    --- Auto-Git Commit ---

--- src/composables/useFileSystem.ts
+++ src/composables/useFileSystem.ts
@@ -6,7 +6,7 @@ export const readBlocks = async () => {
     const BlockArr = Object.keys(requireComponent)
 
     for (let i = 0; i < BlockArr.length; i++) {
-        BlockArr[i].split('/')[3]
+        result.push(BlockArr[i].split('/')[3])
     }
 
 }

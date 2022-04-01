1dcb15bc87f65ee77b9a26413b32e62b03262de2
kromate <akpananthony33@gmail.com>
2022-04-01 16:19:40 +0100

    --- Auto-Git Commit ---

--- src/views/stage/index.vue
+++ src/views/stage/index.vue
@@ -5,7 +5,7 @@
 
 	<div class="flex w-full p-4">
 		<div class="w-40 h-full rounded-md shadow-xl bg-white flex flex-col items-start py-3 px-5">
-			<span class="self-start inline-block px-2 py-1 mb-2 rounded-md cursor-pointer font-bold hover:bg-slate-200 w-full" v-for="n in 12" :key="n">Header</span>
+			<span class="self-start inline-block text-slate-500 px-2 py-1 mb-2 rounded-md cursor-pointer font-bold hover:bg-slate-200 w-full" v-for="n in 12" :key="n">Header</span>
 		</div>
 	</div>
 </template>

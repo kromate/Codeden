7b9253d8af458b4b0ba5f04f941d18d5e97b1c3c
kromate <akpananthony33@gmail.com>
2022-04-01 19:03:24 +0100

    --- Auto-Git Commit ---

--- src/components/stage/SIdeMenu.vue
+++ src/components/stage/SIdeMenu.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="w-48 h-full card flex flex-col items-start py-5 px-5 gap-2">
-		<span class="self-start text-base inline-block text-slate-500 px-2 py-1  rounded-md cursor-pointer font-bold hover:bg-slate-200 w-full" v-for="n in Block" :key="n">{{n}}</span>
+		<span class="self-start text-sm inline-block text-slate-500 px-2 py-1  rounded-md cursor-pointer font-bold hover:bg-slate-200 w-full" v-for="n in Block" :key="n">{{n}}</span>
 	</div>
 </template>
 

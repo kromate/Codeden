ee7e51cf1e26ff9f5ec22c4fc36c3cde02805658
kromate <akpananthony33@gmail.com>
2022-04-01 16:26:10 +0100

    --- Auto-Git Commit ---

--- src/views/stage/index.vue
+++ src/views/stage/index.vue
@@ -4,7 +4,7 @@
 	</nav>
 
 	<div class="flex w-full p-4 gap-4 h-screen">
-		<div class="w-40 h-full rounded-md shadow-xl bg-white flex flex-col items-start py-5 px-5 gap-2">
+		<div class="w-40 h-full card flex flex-col items-start py-5 px-5 gap-2">
 			<span class="self-start inline-block text-slate-500 px-2 py-1  rounded-md cursor-pointer font-bold hover:bg-slate-200 w-full" v-for="n in 12" :key="n">Header</span>
 		</div>
 

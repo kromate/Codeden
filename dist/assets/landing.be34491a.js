c47456c222873b799e8fb0b38e6b83f285579807
kromate <akpananthony33@gmail.com>
2022-04-01 22:15:13 +0100

    adding dist

---
+++ dist/assets/landing.be34491a.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.a6cb465e.js";import{o as n,g as r}from"./vendor.ac79ea00.js";const o={};function a(c,t){return n(),r("div",null,"Landing")}var f=e(o,[["render",a]]);export{f as default};

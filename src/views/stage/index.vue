d3d357ca70f3cb921a7852dd49400471aca2f5a8
kromate <akpananthony33@gmail.com>
2022-04-01 16:23:02 +0100

    --- Auto-Git Commit ---

--- src/views/stage/index.vue
+++ src/views/stage/index.vue
@@ -3,9 +3,12 @@
 		<span class="md:px-5 text-xl font-black leading-none text-white"><span>CodeDen</span><span class="text-white">.</span></span>
 	</nav>
 
-	<div class="flex w-full p-4">
+	<div class="flex w-full p-4 gap-4">
 		<div class="w-40 h-full rounded-md shadow-xl bg-white flex flex-col items-start py-5 px-5 gap-2">
 			<span class="self-start inline-block text-slate-500 px-2 py-1  rounded-md cursor-pointer font-bold hover:bg-slate-200 w-full" v-for="n in 12" :key="n">Header</span>
+		</div>
+
+		<div class="w-full rounded-md shadow-xl">
 		</div>
 	</div>
 </template>

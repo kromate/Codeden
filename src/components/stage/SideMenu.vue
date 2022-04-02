1d256c68086a72f229d3baad9975808a77a4bada
kromate <akpananthony33@gmail.com>
2022-04-02 03:16:03 +0100

    --- Auto-Git Commit ---

--- src/components/stage/SideMenu.vue
+++ src/components/stage/SideMenu.vue
@@ -7,7 +7,7 @@
 			@mouseleave="close"
 		>{{n}}</span>
 	</div>
-	<div class="w-96 h-full card flex flex-col items-start py-5 px-5 gap-2" v-if="currentBlock">
+	<div class="w-96 h-full card flex flex-col items-start py-5 px-5 gap-2 absolute left-48" v-if="currentBlock">
 		{{currentBlock}}
 		<span
 			class="self-start text-sm inline-block text-slate-500 px-2 py-1  rounded-md cursor-pointer font-bold hover:bg-slate-200 w-full"
@@ -25,7 +25,7 @@ const currentBlock = ref('')
 const test = (el)=>{
 	currentBlock.value =	el.target.innerHTML
 }
-const close = (el)=>{
+const close = ()=>{
 	currentBlock.value =''
 }
 const Block = readBlocks()

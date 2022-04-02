59b75b06fa69b706b59f4760e5252c116b1fa944
kromate <akpananthony33@gmail.com>
2022-04-02 03:14:58 +0100

    --- Auto-Git Commit ---

--- src/components/stage/SideMenu.vue
+++ src/components/stage/SideMenu.vue
@@ -7,13 +7,12 @@
 			@mouseleave="close"
 		>{{n}}</span>
 	</div>
-	<div class="w-48 h-full card flex flex-col items-start py-5 px-5 gap-2" v-if="currentBlock">
+	<div class="w-96 h-full card flex flex-col items-start py-5 px-5 gap-2" v-if="currentBlock">
 		{{currentBlock}}
 		<span
 			class="self-start text-sm inline-block text-slate-500 px-2 py-1  rounded-md cursor-pointer font-bold hover:bg-slate-200 w-full"
-			v-for="n in Block" :key="n"
-			@mouseover="test"
-		>{{n}}</span>
+		
+		></span>
 	</div>
 </template>
 

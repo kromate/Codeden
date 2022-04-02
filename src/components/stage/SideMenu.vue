ec00a5b854d7d2d67a24a43db8ec0dc26119036f
kromate <akpananthony33@gmail.com>
2022-04-02 03:09:32 +0100

    --- Auto-Git Commit ---

--- src/components/stage/SideMenu.vue
+++ src/components/stage/SideMenu.vue
@@ -6,7 +6,7 @@
 			@mouseover="test"
 		>{{n}}</span>
 	</div>
-	<div class="w-48 h-full card flex flex-col items-start py-5 px-5 gap-2">
+	<div class="w-48 h-full card flex flex-col items-start py-5 px-5 gap-2" v-if="currentBlock">
 		<span
 			class="self-start text-sm inline-block text-slate-500 px-2 py-1  rounded-md cursor-pointer font-bold hover:bg-slate-200 w-full"
 			v-for="n in Block" :key="n"
@@ -16,8 +16,11 @@
 </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue'
 import { readBlocks } from '../../composables/useFileSystem'
 
+
+const currentBlock = ref('')
 const test = (el)=>{
 	console.log(el.target.innerHTML)
 	

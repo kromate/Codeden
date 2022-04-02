11c50656ac8813e171736a3d32e59c8819de81cb
kromate <akpananthony33@gmail.com>
2022-04-02 03:05:17 +0100

    --- Auto-Git Commit ---

--- src/components/stage/SideMenu.vue
+++ src/components/stage/SideMenu.vue
@@ -1,13 +1,20 @@
 <template>
 	<div class="w-48 h-full card flex flex-col items-start py-5 px-5 gap-2">
-		<span class="self-start text-sm inline-block text-slate-500 px-2 py-1  rounded-md cursor-pointer font-bold hover:bg-slate-200 w-full" v-for="n in Block" :key="n">{{n}}</span>
+		<span
+			class="self-start text-sm inline-block text-slate-500 px-2 py-1  rounded-md cursor-pointer font-bold hover:bg-slate-200 w-full"
+			v-for="n in Block" :key="n"
+			@mouseover="test"
+		>{{n}}</span>
 	</div>
 </template>
 
 <script lang="ts" setup>
 import { readBlocks } from '../../composables/useFileSystem'
-import { useElementHover } from '@vueuse/core'
 
+const test = (el)=>{
+	console.log(el)
+	
+}
 const Block = readBlocks()
 
 console.log(Block)

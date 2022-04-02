3131a77a4be39ce734eb3cceeda23d0c06308291
kromate <akpananthony33@gmail.com>
2022-04-02 20:11:05 +0100

    --- Auto-Git Commit ---

--- src/components/stage/SideMenu.vue
+++ src/components/stage/SideMenu.vue
@@ -11,10 +11,7 @@
 		<div class="bg-bgDark  fixed left-0 top-0 w-[300px]  h-full" v-if="currentBlock" @mouseleave="close">
 			<div class="w-[25rem] h-full card  shadow-xl flex flex-col items-start py-5 px-5 gap-2 absolute left-56 " >
 				{{currentBlock}}
-				<span
-					class="self-start text-sm inline-block text-slate-500 px-2 py-1  rounded-md cursor-pointer font-bold hover:bg-slate-200 w-full"
-		
-				></span>
+				<component is="p"/>
 			</div>
 		</div>
 	

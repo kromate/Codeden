f637e273114ccbb4e2aeb9dbcf686eae4e51cd9f
kromate <akpananthony33@gmail.com>
2022-04-02 03:08:27 +0100

    --- Auto-Git Commit ---

--- src/components/stage/SideMenu.vue
+++ src/components/stage/SideMenu.vue
@@ -6,6 +6,13 @@
 			@mouseover="test"
 		>{{n}}</span>
 	</div>
+	<div class="w-48 h-full card flex flex-col items-start py-5 px-5 gap-2">
+		<span
+			class="self-start text-sm inline-block text-slate-500 px-2 py-1  rounded-md cursor-pointer font-bold hover:bg-slate-200 w-full"
+			v-for="n in Block" :key="n"
+			@mouseover="test"
+		>{{n}}</span>
+	</div>
 </template>
 
 <script lang="ts" setup>

cef294d9123375592120ff4aece3356f64b970fa
kromate <akpananthony33@gmail.com>
2022-04-03 17:30:26 +0100

    --- Auto-Git Commit ---

--- blocks/Navigations/index.vue
+++ blocks/Navigations/index.vue
@@ -1,9 +1,12 @@
 <template>
-	<img src="./one/image.png" alt="Navigation one" class="rounded-md w-full shadow-xl object-cover" v-for="n in 20" :key="n">
+	<draggable>
+		<img src="./one/image.png" alt="Navigation one" class="rounded-md w-full shadow-xl object-cover" v-for="n in 20" :key="n">
+	</draggable>
+	
 </template>
 
 <script lang="ts" setup>
-
+import draggable from 'vuedraggable'
 </script>
 
 <style  scoped>

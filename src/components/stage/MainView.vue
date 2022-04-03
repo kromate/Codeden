2ef142116d05dabfbf82ddd28e00f0be30b4b3a8
kromate <akpananthony33@gmail.com>
2022-04-03 17:47:56 +0100

    --- Auto-Git Commit ---

--- src/components/stage/MainView.vue
+++ src/components/stage/MainView.vue
@@ -1,4 +1,18 @@
 <template>
+	<draggable
+		class="list-group"
+		:list="list2"
+		group="people"
+		@change="checkMove"
+		itemKey="name"
+	>
+		<template #item="{  }">
+			<div class="w-full card ml-56 ">
+
+		
+			</div>
+		</template>
+	</draggable>
 	<div class="w-full card ml-56 ">
 
 		
@@ -6,7 +20,12 @@
 </template>
 
 <script lang="ts" setup>
+import draggable from 'vuedraggable'
+const checkMove = (el)=>{
+	console.log(el)
+}
 
+const list2 = [1,2,3,4,5,6,7,8,9,10]
 </script>
 
 <style  scoped>

dcdbc998059f25e08246fd9e618f77f57eeb803c
kromate <akpananthony33@gmail.com>
2022-04-04 00:58:46 +0100

    --- Auto-Git Commit ---

--- blocks/Headers/index.vue
+++ blocks/Headers/index.vue
@@ -1,10 +1,17 @@
 <template>
-	<div class="text-xl font-bold">
-		This is the Header</div>
+	<img :src="n.img" alt="Navigation one" class="rounded-md w-full shadow-xl object-cover" v-for="n in blockDate" :key="n.index">
 </template>
 
 <script lang="ts" setup>
+//@ts-ignore
+import {getBlockHeaders} from '@/composables/useFileSystem'
+import { ref, onMounted } from 'vue'
 
+const blockDate = ref()
+onMounted(async()=>{
+	blockDate.value = await getBlockHeaders()
+
+} )
 </script>
 
 <style  scoped>
--- src/composables/useFileSystem.ts
+++ src/composables/useFileSystem.ts
@@ -45,3 +45,27 @@ export const getBlockNavigations = async () => {
 
   return result;
 };
+export const getBlockHeaders = async () => {
+  //@ts-ignore
+  const requireComponent = import.meta.glob(`../../blocks/Headers/**`);
+  const BlockArr = Object.keys(requireComponent);
+  let curr = "";
+  let result = [];
+  for (let i = 0; i < BlockArr.length; i++) {
+    let pos = BlockArr[i].split("/")[4];
+    if (curr !== pos && pos !== "index.vue") {
+      let obj = {
+        comp: (await import(`../../blocks/Navigations/${pos}/index.vue`))
+          .default,
+        img: (await import(`../../blocks/Navigations/${pos}/image.png`))
+          .default,
+        index: pos,
+        name: `${BlockArr[i].split("/")[3]}  ${pos}`,
+      };
+      result.push(obj);
+      curr = pos;
+    }
+  }
+
+  return result;
+};

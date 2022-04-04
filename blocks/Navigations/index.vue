4fd550596c9ef8971b70ff8fd797dc12f6d50290
kromate <akpananthony33@gmail.com>
2022-04-04 23:15:18 +0100

    --- Auto-Git Commit ---

--- blocks/Navigations/index.vue
+++ blocks/Navigations/index.vue
@@ -1,6 +1,6 @@
 <template>
 	<div v-for="n in blockDate"  :key="n.index" class="w-full">
-		<img :src="n.img" alt="Navigation one" class="rounded-md w-full shadow-xl object-cover"  v-if="n.imgLoaded" @load="">
+		<img :src="n.img" alt="Navigation one" class="rounded-md w-full shadow-xl object-cover"  v-if="n.imgLoaded" @load="n.imgLoaded = true">
 		<SkeletonLoader v-else height="100px" width="100%" radius="6px"/>
 	</div>
 
@@ -8,11 +8,11 @@
 
 <script lang="ts" setup>
 //@ts-ignore
-import {getBlockNavigations, getBlockObject} from '@/composables/useFileSystem'
+import {getBlockNavigations} from '@/composables/useFileSystem'
 import { ref, onMounted } from 'vue'
 import SkeletonLoader from '../../src/components/core/SkeletonLoader.vue'
 
-const blockDate = ref<getBlockObject>()
+const blockDate = ref()
 onMounted(async()=>{
 	blockDate.value = await getBlockNavigations()
 

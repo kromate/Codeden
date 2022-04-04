bd71d9f0bf9e9d18f66c64057af4fb434169eedd
kromate <akpananthony33@gmail.com>
2022-04-04 23:44:50 +0100

    --- Auto-Git Commit ---

--- blocks/Heros/index.vue
+++ blocks/Heros/index.vue
@@ -1,5 +1,8 @@
 <template>
-	<img :src="n.img" alt="Navigation one" class="rounded-md w-full shadow-xl object-cover" v-for="n in blockDate" :key="n.index">
+	<div v-for="n in blockDate"  :key="n.index" class="w-full">
+		<img :src="n.img" :alt="n.name" class="rounded-md w-full shadow-xl object-cover"  @load="n.imgLoaded = true">
+		<SkeletonLoader v-if="!n.imgLoaded" height="100px" width="100%" radius="6px"/>
+	</div>
 </template>
 
 <script lang="ts" setup>

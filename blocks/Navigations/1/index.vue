7410496490d68e27d0d663beefa9272f7b202296
kromate <akpananthony33@gmail.com>
2022-04-05 00:59:11 +0100

    --- Auto-Git Commit ---

--- blocks/Navigations/1/index.vue
+++ blocks/Navigations/1/index.vue
@@ -1,54 +1,33 @@
 <template>
 
 	<!-- Section 1 -->
-	<section class="relative w-full px-8 text-gray-700 bg-white body-font">
-		<div class="container flex  flex-wrap items-center justify-between py-5 mx-auto md:flex-row max-w-7xl">
-			<a href="#_" class="relative z-10 flex items-center w-auto text-2xl font-extrabold leading-none text-black select-none">tails.</a>
+	<nav class="relative z-50 h-24 select-none"  >
+		<div class="container relative flex flex-wrap items-center justify-between h-24 mx-auto  font-medium border-b border-gray-200 md:overflow-visible">
+			<div class="flex items-center justify-start w-1/4 h-full pr-4">
+				<a href="#_" class=" py-4 md:py-0 flex items-center">
+					
+					<span class="p-1 text-xl font-black leading-none text-gray-900"><span>CodeDen</span><span class="text-indigo-600">.</span></span>
+				</a>
+			</div>
 
-			<nav class="top-0 left-0 z-0 flex items-center justify-center w-full h-full py-5 -ml-0 space-x-5 text-base md:-ml-5 md:py-0 md:absolute">
-				<a href="#_" class="relative font-medium leading-6 text-gray-600 transition duration-150 ease-out hover:text-gray-900" x-data="{ hover: false }" @mouseenter="hover = true" @mouseleave="hover = false">
-					<span class="block">Home</span>
-					<span class="absolute bottom-0 left-0 inline-block w-full h-0.5 -mb-1 overflow-hidden">
-						<span x-show="hover" class="absolute inset-0 inline-block w-full h-1 h-full transform bg-gray-900" x-transition:enter="transition ease duration-200" x-transition:enter-start="scale-0" x-transition:enter-end="scale-100" x-transition:leave="transition ease-out duration-300" x-transition:leave-start="scale-100" x-transition:leave-end="scale-0"></span>
-					</span>
-				</a>
-				<a href="#_" class="relative font-medium leading-6 text-gray-600 transition duration-150 ease-out hover:text-gray-900" x-data="{ hover: false }" @mouseenter="hover = true" @mouseleave="hover = false">
-					<span class="block">Features</span>
-					<span class="absolute bottom-0 left-0 inline-block w-full h-0.5 -mb-1 overflow-hidden">
-						<span x-show="hover" class="absolute inset-0 inline-block w-full h-1 h-full transform bg-gray-900" x-transition:enter="transition ease duration-200" x-transition:enter-start="scale-0" x-transition:enter-end="scale-100" x-transition:leave="transition ease-out duration-300" x-transition:leave-start="scale-100" x-transition:leave-end="scale-0"></span>
-					</span>
-				</a>
-				<a href="#_" class="relative font-medium leading-6 text-gray-600 transition duration-150 ease-out hover:text-gray-900" x-data="{ hover: false }" @mouseenter="hover = true" @mouseleave="hover = false">
-					<span class="block">Pricing</span>
-					<span class="absolute bottom-0 left-0 inline-block w-full h-0.5 -mb-1 overflow-hidden">
-						<span x-show="hover" class="absolute inset-0 inline-block w-full h-1 h-full transform bg-gray-900" x-transition:enter="transition ease duration-200" x-transition:enter-start="scale-0" x-transition:enter-end="scale-100" x-transition:leave="transition ease-out duration-300" x-transition:leave-start="scale-100" x-transition:leave-end="scale-0"></span>
-					</span>
-				</a>
-				<a href="#_" class="relative font-medium leading-6 text-gray-600 transition duration-150 ease-out hover:text-gray-900" x-data="{ hover: false }" @mouseenter="hover = true" @mouseleave="hover = false">
-					<span class="block">Blog</span>
-					<span class="absolute bottom-0 left-0 inline-block w-full h-0.5 -mb-1 overflow-hidden">
-						<span x-show="hover" class="absolute inset-0 inline-block w-full h-1 h-full transform bg-gray-900" x-transition:enter="transition ease duration-200" x-transition:enter-start="scale-0" x-transition:enter-end="scale-100" x-transition:leave="transition ease-out duration-300" x-transition:leave-start="scale-100" x-transition:leave-end="scale-0"></span>
-					</span>
-				</a>
-			</nav>
-
-			<div class="relative z-10 inline-flex items-center space-x-3 md:ml-5 lg:justify-end">
-				<a href="#" class="inline-flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-gray-600 whitespace-no-wrap bg-white border border-gray-200 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:shadow-none">
-					Sign in
-				</a>
-				<span class="inline-flex rounded-md shadow-sm">
-					<a href="#" class="inline-flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-white whitespace-no-wrap bg-blue-600 border border-blue-700 rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-						Sign up
-					</a>
-				</span>
+			<router-link to="#_" v-if="showMenu" class="btn rounded-none py-3 w-full">Get Started</router-link>
+			<router-link to="#_"  class="btn pc py-3">Get Started</router-link>
+			
+			<div @click="showMenu = !showMenu" class="absolute right-0 flex flex-col items-center  justify-center w-10 h-10 bg-white rounded-full cursor-pointer md:hidden hover:bg-gray-100">
+				<svg class="w-6 h-6 text-gray-700" v-if="!showMenu" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor" x-cloak="">
+					<path d="M4 6h16M4 12h16M4 18h16"></path>
+				</svg>
+				<svg class="w-6 h-6 text-gray-700" v-if="showMenu" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" x-cloak="">
+					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+				</svg>
 			</div>
 		</div>
-	</section>
+	</nav>
 
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue'
 
-const hover = ref(false)
+const showMenu = ref(false)
 </script>

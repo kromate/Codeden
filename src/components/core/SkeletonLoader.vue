6a397e4dc80a667fa82d1d9b8b02f923739c7bb6
kromate <akpananthony33@gmail.com>
2022-04-04 22:15:28 +0100

    --- Auto-Git Commit ---

--- src/components/core/SkeletonLoader.vue
+++ src/components/core/SkeletonLoader.vue
@@ -1,13 +1,36 @@
 <template>
-  
+	<div
+		class="pulsate"
+		:style="{ width: width + 'px', height: h + 'px', 'border-radius': b + 'px' }"
+	></div>
 </template>
 
 <script lang="ts" setup>
 defineProps({
-	foo: String
+	width: {
+		default : '120px',
+		type:String
+	}
 })
 </script>
 
 <style  scoped>
-
+.pulsate {
+  max-width: 90vw !important;
+  background: linear-gradient(30deg, #6a3f05, #a2691e, #804b0a, #a56417);
+  background-size: 400% 400%;
+  -webkit-animation: Gradient-04a0d67a 2.25s ease infinite;
+  animation: Gradient-04a0d67a 2s ease infinite;
+}
+@keyframes Gradient {
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+}
 </style>

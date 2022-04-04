8c6b3a4d2f5ad1a9792bdbb7bd7a33e1da21dc03
kromate <akpananthony33@gmail.com>
2022-04-05 00:02:25 +0100

    --- Auto-Git Commit ---

--- blocks/Pricing/index.vue
+++ blocks/Pricing/index.vue
@@ -15,47 +15,32 @@
 
 </template>
 
-<script lang="ts" >
+<script lang="ts" setup>
 import draggable from 'vuedraggable'
-export default {
-	name: 'two-lists',
-	display: 'Two Lists',
-	order: 1,
-	components: {
-		draggable
-	},
-	data() {
-		return {
-			list1: [
-				{ name: 'John', id: 1 },
-				{ name: 'Joao', id: 2 },
-				{ name: 'Jean', id: 3 },
-				{ name: 'Gerard', id: 4 }
-			],
-			list2: [
-				{ name: 'Juan', id: 5 },
-				{ name: 'Edgard', id: 6 },
-				{ name: 'Johnson', id: 7 }
-			]
-		}
-	},
-	methods: {
-		add: function() {
-			this.list.push({ name: 'Juan' })
-		},
-		replace: function() {
-			this.list = [{ name: 'Edgard' }]
-		},
-		clone: function(el) {
-			return {
-				name: el.name + ' cloned'
-			}
-		},
-		log: function(evt) {
-			window.console.log(evt)
-		}
-	}
+
+// const order = 1 
+
+
+const addedComp = [
+	{name:'test', id:6}
+]
+
+// const add= function() {
+// 	this.list.push({ name: 'Juan' })
+// }
+// const replace = function() {
+// 	this.list = [{ name: 'Edgard' }]
+// }
+// const clone = function(el) {
+// 	return {
+// 		name: el.name + ' cloned'
+// 	}
+// }
+const log = function(evt) {
+	window.console.log(evt)
 }
+
+
 </script>
 
 <style  scoped>

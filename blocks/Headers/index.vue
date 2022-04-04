97b16a89324007316d5450745daa733c3d04d2b4
kromate <akpananthony33@gmail.com>
2022-04-04 23:39:36 +0100

    --- Auto-Git Commit ---

--- blocks/Headers/index.vue
+++ blocks/Headers/index.vue
@@ -1,16 +1,19 @@
 <template>
-	<img :src="n.img" alt="Navigation one" class="rounded-md w-full shadow-xl object-cover" v-for="n in blockDate" :key="n.index">
+	<div v-for="n in blockDate"  :key="n.index" class="w-full">
+		<img :src="n.img" alt="Navigation one" class="rounded-md w-full shadow-xl object-cover"  @load="n.imgLoaded = true">
+		<SkeletonLoader v-if="!n.imgLoaded" height="100px" width="100%" radius="6px"/>
+	</div>
 </template>
 
 <script lang="ts" setup>
 //@ts-ignore
 import {getBlockHeaders} from '@/composables/useFileSystem'
 import { ref, onMounted } from 'vue'
+import SkeletonLoader from '../../src/components/core/SkeletonLoader.vue'
 
 const blockDate = ref()
 onMounted(async()=>{
 	blockDate.value = await getBlockHeaders()
-
 } )
 </script>
 
--- blocks/Navigations/index.vue
+++ blocks/Navigations/index.vue
@@ -1,44 +1,23 @@
 <template>
 	<div v-for="n in blockDate"  :key="n.index" class="w-full">
 		<img :src="n.img" alt="Navigation one" class="rounded-md w-full shadow-xl object-cover"  @load="n.imgLoaded = true">
-	
 		<SkeletonLoader v-if="!n.imgLoaded" height="100px" width="100%" radius="6px"/>
 	</div>
-
 </template>
 
 <script lang="jsx" setup>
 //@ts-ignore
 import {getBlockNavigations} from '@/composables/useFileSystem'
 import { ref, onMounted } from 'vue'
-import SkeletonLoader from '../../src/components/core/SkeletonLoader.vue'
+import SkeletonLoader from '@/components/core/SkeletonLoader.vue'
 
 const blockDate = ref()
 onMounted(async()=>{
 	blockDate.value = await getBlockNavigations()
 })
-
-
-
 </script>
 
 <style  scoped>
 
 </style>
 
-
-    <!-- imgLoad() {
-      var image = document.querySelector("img");
-      console.log(image.complete);
-      if (image.complete && image.naturalHeight >= 0) {
-        this.loaded = true;
-        clearInterval(this.inter);
-      }
-    }, 
-	
-	    show() {
-      this.inter = setInterval(() => {
-        this.imgLoad();
-      }, 1000);
-    },
-	-->

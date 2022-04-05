7fc6d3c6b7b52a1db63d3f7a9b19388c0e8e31eb
kromate <akpananthony33@gmail.com>
2022-04-05 16:52:27 +0100

    --- Auto-Git Commit ---

---
+++ blocks/Logos/2/index.vue
@@ -0,0 +1,27 @@
+<template>
+	<section class="bg-white pt-7 pb-14">
+		<div class="container px-8 mx-auto sm:px-12 lg:px-20">
+			<h1 class="text-sm font-bold tracking-wide text-center text-gray-800 uppercase mb-7">Trusted by top-leading companies.</h1>
+			<div class="flex grid items-center justify-center grid-cols-4 grid-cols-12 gap-y-8">
+				<div class="flex items-center justify-center col-span-6 sm:col-span-4 md:col-span-3 xl:col-span-2">
+					<img src="https://cdn.devdojo.com/tails/images/disney-plus.svg" alt="Disney Plus" class="block object-contain h-12">
+				</div>
+				<div class="flex items-center justify-center col-span-6 sm:col-span-4 md:col-span-3 xl:col-span-2">
+					<img src="https://cdn.devdojo.com/tails/images/google.svg" alt="Google" class="block object-contain h-9">
+				</div>
+				<div class="flex items-center justify-center col-span-6 sm:col-span-4 md:col-span-3 xl:col-span-2">
+					<img src="https://cdn.devdojo.com/tails/images/hubspot.svg" alt="Hubspot" class="block object-contain h-9">
+				</div>
+				<div class="flex items-center justify-center col-span-6 sm:col-span-4 md:col-span-3 xl:col-span-2">
+					<img src="https://cdn.devdojo.com/tails/images/youtube.svg" alt="Youtube" class="block object-contain h-7 lg:h-8">
+				</div>
+				<div class="flex items-center justify-center col-span-6 sm:col-span-4 md:col-span-6 xl:col-span-2">
+					<img src="https://cdn.devdojo.com/tails/images/slack.svg" alt="Slack" class="block object-contain h-9">
+				</div>
+				<div class="flex items-center justify-center col-span-6 sm:col-span-4 md:col-span-6 xl:col-span-2">
+					<img src="https://cdn.devdojo.com/tails/images/shopify.svg" alt="Shopify" class="block object-contain h-9">
+				</div>
+			</div>
+		</div>
+	</section>s
+</template>

b21152344b46befa2f1e8b032da76fe70c5f60fa
kromate <akpananthony33@gmail.com>
2022-04-05 02:20:20 +0100

    --- Auto-Git Commit ---

--- blocks/Intergrations/index.vue
+++ blocks/Intergrations/index.vue
@@ -1,12 +1,33 @@
 <template>
-	<div class="text-xl font-bold">
-		This is the Intergration</div>
+	<draggable
+		class="w-full"
+		:list="blockDate"
+		group="people"
+		itemKey="name"
+	>
+		<template #item="{ element }">
+			<div class="w-full mt-5">
+				<img :src="element.img" :alt="element.name" class="rounded-md w-full shadow-xl object-cover"  @load="element.imgLoaded = true">
+				<SkeletonLoader v-if="!element.imgLoaded" height="100px" width="100%" radius="6px"/>
+			</div>
+		</template>
+	</draggable>
 </template>
 
-<script lang="ts" setup>
+<script lang="jsx" setup>
+//@ts-ignore
+import {getBlockTeams} from '@/composables/useFileSystem'
+import { ref, onMounted } from 'vue'
+import draggable from 'vuedraggable'
+import SkeletonLoader from '@/components/core/SkeletonLoader.vue'
 
+const blockDate = ref()
+onMounted(async()=>{
+	blockDate.value = await getBlockTeams()
+})
 </script>
 
 <style  scoped>
 
 </style>
+

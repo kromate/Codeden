b2d4450d273db108c955b68f55e7c355e55677d6
kromate <akpananthony33@gmail.com>
2022-04-05 16:50:45 +0100

    --- Auto-Git Commit ---

--- blocks/Navigations/1/index.vue
+++ blocks/Navigations/1/index.vue
@@ -1,7 +1,7 @@
 <template>
 
 	<!-- Section 1 -->
-	<section class="relative w-full px-8 text-gray-700 bg-white body-font rounded-md">
+	<section class="relative w-full px-8 text-gray-700 bg-white body-font ">
 		<div class="container flex flex-col flex-wrap items-center justify-between py-5 mx-auto md:flex-row max-w-7xl">
 			<a href="#_" class="relative  flex items-center w-auto text-xl font-extrabold leading-none text-black select-none">tails.</a>
 

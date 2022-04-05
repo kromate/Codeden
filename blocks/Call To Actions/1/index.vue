644b69ae585973dd1e8945eeb97cc3c54fd1dff9
kromate <akpananthony33@gmail.com>
2022-04-05 17:37:10 +0100

    --- Auto-Git Commit ---

---
+++ blocks/Call To Actions/1/index.vue
@@ -0,0 +1,16 @@
+<template>
+	<section class="py-20 bg-white">
+		<div class="px-4 mx-auto text-center max-w-7xl sm:px-6 lg:px-8">
+			<h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl md:text-5xl xl:text-6xl">
+				The New Standard for Design
+			</h2>
+			<p class="max-w-md mx-auto mt-3 text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
+				Use our award-winning tools to help you maximize your profits. We've uncovered the correct recipe for converting visitors into customers.
+			</p>
+			<div class="flex justify-center mt-8 space-x-3">
+				<a href="#" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow hover:bg-indigo-700">Sign Up Today</a>
+				<a href="#" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-indigo-700 bg-indigo-100 border border-transparent rounded-md hover:bg-indigo-200">Learn more</a>
+			</div>
+		</div>
+	</section>
+</template>

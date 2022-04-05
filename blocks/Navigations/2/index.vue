6028f8512c47c2cbef4ed146014476b1342c7cb8
kromate <akpananthony33@gmail.com>
2022-04-05 01:14:57 +0100

    --- Auto-Git Commit ---

--- blocks/Navigations/2/index.vue
+++ blocks/Navigations/2/index.vue
@@ -17,7 +17,7 @@
 				<a href="#" class="text-base font-medium leading-6 text-gray-600 whitespace-no-wrap transition duration-150 ease-in-out hover:text-gray-900">
 					Sign in
 				</a>
-				<a href="#" class="inline-flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-white whitespace-no-wrap bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600">
+				<a href="#" class="inline-flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-white whitespace-no-wrap bg-primary border border-transparent rounded-md shadow-sm hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600">
 					Sign up
 				</a>
 			</div>

ff1230011bb39e92fa2f0bdf98fa0aa10798d253
kromate <akpananthony33@gmail.com>
2022-04-05 03:09:52 +0100

    --- Auto-Git Commit ---

--- blocks/Navigations/1/index.vue
+++ blocks/Navigations/1/index.vue
@@ -28,8 +28,8 @@
 				<a href="#" class="mr-5 inline-flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-gray-600 whitespace-no-wrap bg-white border border-gray-200 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:shadow-none">
 					Sign in
 				</a>
-				<span class="inline-flex rounded-md shadow-sm">
-					<a href="#" class="inline-flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-white whitespace-no-wrap bg-blue-600 border border-blue-700 rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+				<span class="inline-flex rounded-md shadow-sm bg-blue">
+					<a href="#" class="flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-white bg-primary rounded-md">
 						Sign up
 					</a>
 				</span>

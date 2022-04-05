a40c7aab4c15d0904ee1d6b10e14454ac90e8d4c
kromate <akpananthony33@gmail.com>
2022-04-05 02:23:50 +0100

    --- Auto-Git Commit ---

--- blocks/Features/index.vue
+++ blocks/Features/index.vue
@@ -1,10 +1,30 @@
 <template>
-	<div class="text-xl font-bold">
-		This is the Features</div>
+	<draggable
+		class="w-full"
+		:list="blockDate"
+		group="people"
+		itemKey="name"
+	>
+		<template #item="{ element }">
+			<div class="w-full mt-5">
+				<img :src="element.img" :alt="element.name" class="rounded-md w-full shadow-xl object-cover"  @load="element.imgLoaded = true">
+				<SkeletonLoader v-if="!element.imgLoaded" height="100px" width="100%" radius="6px"/>
+			</div>
+		</template>
+	</draggable>
 </template>
 
 <script lang="ts" setup>
+//@ts-ignore
+import {getBlockHeaders} from '@/composables/useFileSystem'
+import draggable from 'vuedraggable'
+import { ref, onMounted } from 'vue'
+import SkeletonLoader from '@/components/core/SkeletonLoader.vue'
 
+const blockDate = ref()
+onMounted(async()=>{
+	blockDate.value = await getBlockHeaders()
+} )
 </script>
 
 <style  scoped>

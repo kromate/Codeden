695a397c82a2faaa928083af37927a4cbda93cbc
kromate <akpananthony33@gmail.com>
2022-04-05 17:45:35 +0100

    --- Auto-Git Commit ---

---
+++ blocks/FAQs/1/index.vue
@@ -0,0 +1,36 @@
+<template>
+	<section class="relative py-16 bg-white min-w-screen animation-fade animation-delay">
+		<div class="container px-0 px-8 mx-auto sm:px-12 xl:px-5">
+			<p class="text-xs font-bold text-left text-pink-500 uppercase sm:mx-6 sm:text-center sm:text-normal sm:font-bold">
+				Got a Question? We’ve got answers.
+			</p>
+			<h3 class="mt-1 text-2xl font-bold text-left text-gray-800 sm:mx-6 sm:text-3xl md:text-4xl lg:text-5xl sm:text-center sm:mx-0">
+				Frequently Asked Questions
+			</h3>
+			<div class="w-full px-6 py-6 mx-auto mt-10 bg-white border border-gray-200 rounded-lg sm:px-8 md:px-12 sm:py-8 sm:shadow lg:w-5/6 xl:w-2/3">
+				<h3 class="text-lg font-bold text-purple-500 sm:text-xl md:text-2xl">How does it work?</h3>
+				<p class="mt-2 text-base text-gray-600 sm:text-lg md:text-normal">
+					Our platform works with your content to provides insights and metrics on how you can grow your business and scale your infastructure.
+				</p>
+			</div>
+			<div class="w-full px-6 py-6 mx-auto mt-10 bg-white border border-gray-200 rounded-lg sm:px-8 md:px-12 sm:py-8 sm:shadow lg:w-5/6 xl:w-2/3">
+				<h3 class="text-lg font-bold text-purple-500 sm:text-xl md:text-2xl">Do you offer team pricing?</h3>
+				<p class="mt-2 text-base text-gray-600 sm:text-lg md:text-normal">
+					Yes, we do! Team pricing is available for any plan. You can take advantage of 30% off for signing up for team pricing of 10 users or more.
+				</p>
+			</div>
+			<div class="w-full px-6 py-6 mx-auto mt-10 bg-white border border-gray-200 rounded-lg sm:px-8 md:px-12 sm:py-8 sm:shadow lg:w-5/6 xl:w-2/3">
+				<h3 class="text-lg font-bold text-purple-500 sm:text-xl md:text-2xl">How do I make changes and configure my site?</h3>
+				<p class="mt-2 text-base text-gray-600 sm:text-lg md:text-normal">
+					You can easily change your site settings inside of your site dashboard by clicking the top right menu and clicking the settings button.
+				</p>
+			</div>
+			<div class="w-full px-6 py-6 mx-auto mt-10 bg-white border border-gray-200 rounded-lg sm:px-8 md:px-12 sm:py-8 sm:shadow lg:w-5/6 xl:w-2/3">
+				<h3 class="text-lg font-bold text-purple-500 sm:text-xl md:text-2xl">How do I add a custom domain?</h3>
+				<p class="mt-2 text-base text-gray-600 sm:text-lg md:text-normal">
+					You can easily change your site settings inside of your site dashboard by clicking the top right menu and clicking the settings button.
+				</p>
+			</div>
+		</div>
+	</section>
+</template>

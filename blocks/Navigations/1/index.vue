ed756ffc20d97a96df3d8d94e7dc082a6cff0a31
kromate <akpananthony33@gmail.com>
2022-04-05 02:53:37 +0100

    --- Auto-Git Commit ---

--- blocks/Navigations/1/index.vue
+++ blocks/Navigations/1/index.vue
@@ -5,7 +5,7 @@
 		<div class="container flex flex-col flex-wrap items-center justify-between py-5 mx-auto md:flex-row max-w-7xl">
 			<a href="#_" class="relative z-10 flex items-center w-auto text-2xl font-extrabold leading-none text-black select-none">tails.</a>
 
-			<nav class="top-0 left-0 z-0 flex items-center justify-center w-auto h-full py-5 -ml-0 space-x-5 text-base md:-ml-5 md:py-0 md:absolute">
+			<nav class="top-0 left-0 z-0 flex items-center justify-center w-auto h-full py-5 -ml-0 space-x-5 text-base  md:absolute">
 				<a href="#_" class="relative font-medium leading-6 text-gray-600 transition duration-150 ease-out hover:text-gray-900" x-data="{ hover: false }" @mouseenter="hover = true" @mouseleave="hover = false">
 					<span class="block">Home</span>
 				

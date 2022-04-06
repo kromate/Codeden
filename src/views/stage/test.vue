fa96aab1ff90baecc83f3fab7b624d6b09cfdc78
kromate <akpananthony33@gmail.com>
2022-04-06 17:58:01 +0100

    --- Auto-Git Commit ---

--- src/views/stage/test.vue
+++ src/views/stage/test.vue
@@ -1,9 +1,11 @@
 <template>
-	<component :is="stagedComp.value" class="hover:rounded-lg"/>
+	<component :is="elem.comp" class="hover:rounded-lg" v-for="elem in stagedComp" :key="elem.name"/>
 </template>
 
 <script setup>
 import {stagedComp} from '../../composables/useStage'
+
+console.log(stagedComp.value)
 
 
 </script>

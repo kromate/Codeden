75ff22d0f4f2d30683c5103a5347d321f4b65754
kromate <akpananthony33@gmail.com>
2022-04-06 11:13:02 +0100

    --- Auto-Git Commit ---

--- src/composables/useStage.ts
+++ src/composables/useStage.ts
@@ -2,7 +2,28 @@ import { ref } from 'vue';
 
 export const stagedComp = ref([]);
 
-
 export const delBlock = (index) => {
   stagedComp.value.splice(index, 1);
+};
+
+export const serialize = (key, value) => {
+  if (typeof value === "function") {
+    return value.toString();
+  }
+  return value;
+};
+
+export const deserialize = (key, value) => {
+  if (value && typeof value === "string" && value.substr(0, 8) == "function") {
+    var startBody = value.indexOf("{") + 1;
+    var endBody = value.lastIndexOf("}");
+    var startArgs = value.indexOf("(") + 1;
+    var endArgs = value.indexOf(")");
+
+    return new Function(
+      value.substring(startArgs, endArgs),
+      value.substring(startBody, endBody)
+    );
+  }
+  return value;
 };

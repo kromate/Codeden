dfa9683746c799815b9a9ec502f181f0e9caa642
kromate <akpananthony33@gmail.com>
2022-04-06 16:46:39 +0100

    --- Auto-Git Commit ---

--- src/composables/useStage.ts
+++ src/composables/useStage.ts
@@ -6,20 +6,14 @@ export const delBlock = (index) => {
   stagedComp.value.splice(index, 1);
 };
 
-export class elemObject {
-  constructor(comp, pos, name) {
-    this.comp = comp;
-    this.pos = pos;
-  }
-}
-// export const elemObject = function (comp, pos, name) {
-//   let obj = {
-//     comp: comp,
-//     img: null,
-//     index: pos,
-//     compLoaded: false,
-//     imgLoaded: false,
-//     name: name,
-//   };
-//   //   return obj;
-// };
+export const elemObject = function (comp, pos, name) {
+  let obj = {
+    comp: comp,
+    img: null,
+    index: pos,
+    compLoaded: false,
+    imgLoaded: false,
+    name: name,
+  };
+  return obj;
+};

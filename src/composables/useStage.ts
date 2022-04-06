a33f6d332954bec75a5b0ca7be64f9909a574e5e
kromate <akpananthony33@gmail.com>
2022-04-06 16:34:51 +0100

    --- Auto-Git Commit ---

--- src/composables/useStage.ts
+++ src/composables/useStage.ts
@@ -5,3 +5,15 @@ export const stagedComp = ref([]);
 export const delBlock = (index) => {
   stagedComp.value.splice(index, 1);
 };
+
+export const elemObject = function (comp, pos, name) {
+  let obj = {
+    comp: comp,
+    img: null,
+    index: pos,
+    compLoaded: false,
+    imgLoaded: false,
+    name: name,
+  };
+  return obj;
+};

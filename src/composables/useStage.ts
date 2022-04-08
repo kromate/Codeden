518a7a59756d2ec885fec405ee170ac327960688
kromate <akpananthony33@gmail.com>
2022-04-08 03:00:18 +0100

    --- Auto-Git Commit ---

--- src/composables/useStage.ts
+++ src/composables/useStage.ts
@@ -57,7 +57,7 @@ export const loadSavedComp = () => {
     newArr.pop();
     const elemName = newArr.join(" ");
     console.log(`../../../blocks/${elemName}/${elemArrPos}/index.vue`);
-    import(`../../../blocks/Headers/${elemArrPos}/index.vue`).then((d) => {
+    import(`../../blocks/Headers/1/index.vue`).then((d) => {
       //@ts-ignore
       stagedComp.value.push(new elemObject(d.default, elemArrPos, elem));
     });

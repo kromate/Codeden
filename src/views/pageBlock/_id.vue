db9c6379a68db4e8595d4e6f2895097ed70a6ff0
kromate <akpananthony33@gmail.com>
2022-04-08 04:08:32 +0100

    --- Auto-Git Commit ---

---
+++ src/views/pageBlock/_id.vue
@@ -0,0 +1,18 @@
+<template>
+	<DefaultLayout>
+		<component :is="elem.comp"  v-for="elem in stagedComp" :key="elem.name"/>
+	</DefaultLayout>
+</template>
+
+<script setup>
+import {stagedComp, loadSavedComp} from '../../composables/useStage'
+import { onMounted } from 'vue'
+import DefaultLayout from '../../layouts/defaultLayout.vue'
+import { useRoute } from 'vue-router'
+onMounted(loadSavedComp)
+
+
+
+const id = useRoute().params.id
+console.log(id)
+</script>

d473129f894c171ac88c2a6857338f9f955017c6
kromate <akpananthony33@gmail.com>
2022-05-02 02:54:34 +0100

    --- Auto-Git Commit ---

---
+++ src/components/core/modal/ModalBase.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>
+		<component :is="modals[id]" v-for="id in stack" :key="id" :modal="id" />
+	</div>
+</template>
+
+<script setup lang="ts">
+
+import { modal } from '@'
+
+
+const { stack, modals } = modal
+
+
+</script>

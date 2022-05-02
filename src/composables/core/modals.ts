a1a2c3fe391ae4abda34bc1eedb2cc626c4af0c1
kromate <akpananthony33@gmail.com>
2022-05-02 03:02:56 +0100

    --- Auto-Git Commit ---

--- src/composables/core/modals.ts
+++ src/composables/core/modals.ts
@@ -1,29 +1,29 @@
-
+import { ref } from 'vue'
 import { useModal } from './modal'
-import MakePayment from '@app/components/modals/payments/MakePayment.vue'
+import MakePayment from '../../components/modals/test.vue'
 
 
-type AccountTypes = 'BuyCoins' | 'WithdrawCoins'
+type AccountTypes = 'MakePayment'
 // type MenuTypes = 'RightSidebar' | 'Sidebar'
 // type SessionTypes = 'CreateSession' | 'Ratings'
 // type PaymentTypes = 'MakePayment'
 // type ReportTypes = 'ReportUser' | 'ReportQuestion' | 'ReportAnswer'
 // type QuestionTypes = 'AskQuestions'
 
-const AccountModals = { BuyCoins } as Record<AccountTypes, any>
+const AccountModals = { MakePayment } as Record<AccountTypes, any>
 
 
-export const modal = useModal(ssrRef([] as string[]))
+export const modal = useModal(ref([] as string[]))
 const accountModal = modal.register('Account', AccountModals)
-const menuModal = modal.register('Menu', MenuModals)
-const sessionModal = modal.register('Session', SessionModals)
-const paymentModal = modal.register('Payment', PaymentModals)
-const reportModal = modal.register('Report', ReportModals)
-const questionModals = modal.register('Question', QuestionModals)
+// const menuModal = modal.register('Menu', MenuModals)
+// const sessionModal = modal.register('Session', SessionModals)
+// const paymentModal = modal.register('Payment', PaymentModals)
+// const reportModal = modal.register('Report', ReportModals)
+// const questionModals = modal.register('Question', QuestionModals)
 
 export const useAccountModal = () => accountModal
-export const useMenuModal = () => menuModal
-export const useSessionModal = () => sessionModal
-export const usePaymentModal = () => paymentModal
-export const useReportModal = () => reportModal
-export const useQuestionsModal = () => questionModals
+// export const useMenuModal = () => menuModal
+// export const useSessionModal = () => sessionModal
+// export const usePaymentModal = () => paymentModal
+// export const useReportModal = () => reportModal
+// export const useQuestionsModal = () => questionModals

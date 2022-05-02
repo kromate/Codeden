133cf3b996cae40b96f8e46fe4cca340cbe2758f
kromate <akpananthony33@gmail.com>
2022-05-02 16:37:22 +0100

    --- Auto-Git Commit ---

--- src/components/core/modal/Modal.vue
+++ src/components/core/modal/Modal.vue
@@ -1,7 +1,10 @@
 <template>
 
 	<transition name="slide" appear :duration="500">
-		<div  class="bg transition-all">
+		<div 	:close="closeModal"
+			:close-on-background="closeOnBackground"
+			background-class="modal-background"
+			modal-class="modal-inner" class="bg transition-all">
 			<transition  appear @before-enter="beforeEnter" @leave="onLeave"
 				@enter="enter"
 			>
@@ -11,40 +14,65 @@
 	</transition>
 </template>
 
-<script lang="ts" setup>
+<script lang="ts" >
 import gsap from 'gsap'
 
-
-
-const timeline = gsap.timeline({defaults:{duration:0.5}})
-const beforeEnter = (el) => {
+export default {
+	name: 'Modal',
+	props: {
+		modal: {
+			type: String,
+			required: true
+		},
+		close: {
+			type: Function,
+			required: false,
+			default: () => {
+			}
+		},
+		hideSeparator: {
+			type: Boolean,
+			required: false,
+			default: false
+		},
+		closeOnBackground: {
+			type: Boolean,
+			required: false,
+			default: false
+		}
+	},
+	setup (props) {
+		const timeline = gsap.timeline({defaults:{duration:0.5}})
+		const beforeEnter = (el) => {
 			  el.style.opacity = 0
-	el.style.transform = 'scale(0.5)'
-}
-const enter = (el, done) => {
-	timeline.to(el, {
-		opacity: 1,
-		y: 0,
-		scale:1,
-		duration: 0.35,
-		onComplete: done,
-	},)
-}
-const onLeave=(el, done)=> {
-	console.log(el)
+			el.style.transform = 'scale(0.5)'
+		}
+		const enter = (el, done) => {
+			timeline.to(el, {
+				opacity: 1,
+				y: 0,
+				scale:1,
+				duration: 0.35,
+				onComplete: done,
+			},)
+		}
+		const onLeave=(el, done)=> {
+			console.log(el)
 			
-	gsap.to(el, {
-		opacity: 0,
-		y: 0,
-		scale:0.1,
-		duration: 0.35,
-		onComplete: done,
-	},)
+			gsap.to(el, {
+				opacity: 0,
+				y: 0,
+				scale:0.1,
+				duration: 0.35,
+				onComplete: done,
+			},)
+		}
+	}
 }
-
-
-
 </script>
+
+
+
 
 <style scoped>
 

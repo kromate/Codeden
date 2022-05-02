dde5fcb73aa601cb8a03610506fbc182767a7120
kromate <akpananthony33@gmail.com>
2022-05-02 18:33:55 +0100

    --- Auto-Git Commit ---

--- src/components/modals/stage/SaveBlock.vue
+++ src/components/modals/stage/SaveBlock.vue
@@ -1,6 +1,6 @@
 <template>
 	<Modal modal="$atts.modal">
-		<div class="h-screen bg-white max-w-[79%]  w-[190rem] right-0 fixed py-9 px-12 ">
+		<div class="h-60 bg-white max-w-[79%]  py-9 px-12 ">
 			<div class="flex w-full justify-between">
 				<div class="flex items-center">
 					<div class="btn-outline-2 mr-6 cursor-pointer" @click="close()">

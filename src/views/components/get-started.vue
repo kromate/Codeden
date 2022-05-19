d3a616a42900c1b57a5b0ebfb621f7902788d257
kromate <akpananthony33@gmail.com>
2022-05-19 14:55:40 +0100

    update

---
+++ src/views/components/get-started.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="w-full h-full ">
+		<sidebar/>
+	</div>
+</template>
+
+<script setup lang="ts">
+import sidebar from '@/components/components/sidebar.vue'
+
+</script>
+
+<style scoped>
+
+</style>

06f29cf8e368b3a49244bf8e7c041d993cf7db5e
kromate <akpananthony33@gmail.com>
2022-05-19 15:14:37 +0100

    logo

--- src/components/components/sidebar.vue
+++ src/components/components/sidebar.vue
@@ -1,52 +1,55 @@
 <template>
 	<div class="fixed inset-0 z-50 transition-opacity bg-black opacity-50 xl:hidden hidden"></div>
 
-	<div class="fixed inset-y-0 left-0 z-50 flex flex-col w-64 h-screen px-4 py-8 overflow-y-auto transition duration-200 transform bg-white border-gray-200 shadow-lg xl:translate-x-0 xl:static xl:inset-0 -translate-x-full ease-in"><a href="/" class="inline-flex items-center text-xl font-bold text-gray-700 transition-colors duration-200 transform md:text-2xl hover:text-blue-500"><img src="data:image/svg+xml;base64,Cjxzdmcgdmlld0JveD0iMCAwIDIwOSAxODAiIGZpbGw9Im5vbmUiCiAgICB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPgogICAgPHBhdGggZD0iTTg3Ljg0MyAxMDguNDg3TDkwLjYxNyAxMDEuNTUzQzkxLjMxMiA5OS41NzEgOTMuNDMzIDk4LjQ3NiA5NS40NTEgOTkuMDU3TDEzMi4xNDIgMTEwLjA3MkwxNzEuNDA4IDQ1Ljk2Mkw3MS41OTggMTcuNDM0TDg3Ljg0MyAxMDguNDg3Wk0xNzMuNTQ3IDM4LjA3N1YwTDEzOC40MDIgMjYuMzQ5TDE3My41NDcgMzguMDc3Wk0yMDguNTc0IDIyLjYyNDVMMTgxLjQ3MiAwLjQzNTc5OVYyMC4yNDcyTDIwOC41NzQgMjIuNjI0NVpNMTcyLjQ3NyA1OS40MzRMMTQyLjcyMSAxMDguMTdMMTY5LjU4NSAxMDAuMDA4TDE3Mi40NzcgNTkuNDM0Wk0xMjYuNTk0IDExNi44ODdMOTYuNjQgMTA3Ljg5MkM4Ny44MDQgMTI5Ljk2MiA5My45ODUgMTE0LjkwNiA2Ni41NjYgMTc5LjcyOEwxMjYuNTk0IDExNi44ODdaTTYzLjM5NiAxNi42MDE5TDAuNjczNjM0IDI5Ljc5Nkw3NS45NTcgODYuMTc5TDYzLjM5NiAxNi42MDE5Wk01Mi43Nzc0IDc4LjgwOUwxOS42MTMzIDUzLjkyNkwwLjYzNDAzMyA2OC41ODdMNTIuNzc3NCA3OC44MDlaIiBmaWxsPSIjM0I4MkY2Ii8+Cjwvc3ZnPgo=" alt="logo" class="mr-2 w-7 h-7"> Meraki <span class="text-blue-500">UI</span></a> <nav class="mt-6"><div class="flex items-center justify-between px-4 py-2 mt-1 rounded-md focus:outline-none text-gray-500 hover:text-blue-500 transition-colors duration-200 transform"><button class="transition-colors duration-200 ease-in-out focus:outline-none w-full text-left hover:underline">
-		Alerts
-	</button> <!----></div><div class="flex items-center justify-between px-4 py-2 mt-1 rounded-md focus:outline-none text-gray-500 hover:text-blue-500 transition-colors duration-200 transform"><button class="transition-colors duration-200 ease-in-out focus:outline-none w-full text-left hover:underline">
-		Authentication
-	</button> <!----></div><div class="flex items-center justify-between px-4 py-2 mt-1 rounded-md focus:outline-none text-gray-500 hover:text-blue-500 transition-colors duration-200 transform"><button class="transition-colors duration-200 ease-in-out focus:outline-none w-full text-left hover:underline">
-		Breadcrumbs
-	</button> <!----></div><div class="flex items-center justify-between px-4 py-2 mt-1 rounded-md focus:outline-none text-gray-500 hover:text-blue-500 transition-colors duration-200 transform"><button class="transition-colors duration-200 ease-in-out focus:outline-none w-full text-left hover:underline">
-		Buttons
-	</button> <!----></div><div class="flex items-center justify-between px-4 py-2 mt-1 rounded-md focus:outline-none text-gray-500 hover:text-blue-500 transition-colors duration-200 transform"><button class="transition-colors duration-200 ease-in-out focus:outline-none w-full text-left hover:underline">
-		Cards
-	</button> <!----></div><div class="flex items-center justify-between px-4 py-2 mt-1 rounded-md focus:outline-none text-gray-500 hover:text-blue-500 transition-colors duration-200 transform"><button class="transition-colors duration-200 ease-in-out focus:outline-none w-full text-left hover:underline">
-		CTA
-	</button> <!----></div><div class="flex items-center justify-between px-4 py-2 mt-1 rounded-md focus:outline-none text-gray-500 hover:text-blue-500 transition-colors duration-200 transform"><button class="transition-colors duration-200 ease-in-out focus:outline-none w-full text-left hover:underline">
-		FAQ
-	</button> <!----></div><div class="flex items-center justify-between px-4 py-2 mt-1 rounded-md focus:outline-none text-gray-500 hover:text-blue-500 transition-colors duration-200 transform"><button class="transition-colors duration-200 ease-in-out focus:outline-none w-full text-left hover:underline">
-		Features
-	</button> <!----></div><div class="flex items-center justify-between px-4 py-2 mt-1 rounded-md focus:outline-none text-gray-500 hover:text-blue-500 transition-colors duration-200 transform"><button class="transition-colors duration-200 ease-in-out focus:outline-none w-full text-left hover:underline">
-		Forms
-	</button> <!----></div><div class="flex items-center justify-between px-4 py-2 mt-1 rounded-md focus:outline-none text-gray-500 hover:text-blue-500 transition-colors duration-200 transform"><button class="transition-colors duration-200 ease-in-out focus:outline-none w-full text-left hover:underline">
-		Dropdowns
-	</button> <!----></div><div class="flex items-center justify-between px-4 py-2 mt-1 rounded-md focus:outline-none text-gray-500 hover:text-blue-500 transition-colors duration-200 transform"><button class="transition-colors duration-200 ease-in-out focus:outline-none w-full text-left hover:underline">
-		Navbars
-	</button> <!----></div><div class="flex items-center justify-between px-4 py-2 mt-1 rounded-md focus:outline-none text-gray-500 hover:text-blue-500 transition-colors duration-200 transform"><button class="transition-colors duration-200 ease-in-out focus:outline-none w-full text-left hover:underline">
-		Heros
-	</button> <!----></div><div class="flex items-center justify-between px-4 py-2 mt-1 rounded-md focus:outline-none text-gray-500 hover:text-blue-500 transition-colors duration-200 transform"><button class="transition-colors duration-200 ease-in-out focus:outline-none w-full text-left hover:underline">
-		Sidebar
-	</button> <!----></div><div class="flex items-center justify-between px-4 py-2 mt-1 rounded-md focus:outline-none text-gray-500 hover:text-blue-500 transition-colors duration-200 transform"><button class="transition-colors duration-200 ease-in-out focus:outline-none w-full text-left hover:underline">
-		Sections
-	</button> <!----></div><div class="flex items-center justify-between px-4 py-2 mt-1 rounded-md focus:outline-none text-gray-500 hover:text-blue-500 transition-colors duration-200 transform"><button class="transition-colors duration-200 ease-in-out focus:outline-none w-full text-left hover:underline">
-		Tabs
-	</button> <!----></div><div class="flex items-center justify-between px-4 py-2 mt-1 rounded-md focus:outline-none text-gray-500 hover:text-blue-500 transition-colors duration-200 transform"><button class="transition-colors duration-200 ease-in-out focus:outline-none w-full text-left hover:underline">
-		Teams
-	</button> <!----></div><div class="flex items-center justify-between px-4 py-2 mt-1 rounded-md focus:outline-none text-gray-500 hover:text-blue-500 transition-colors duration-200 transform"><button class="transition-colors duration-200 ease-in-out focus:outline-none w-full text-left hover:underline">
-		Pricing
-	</button> <!----></div><div class="flex items-center justify-between px-4 py-2 mt-1 rounded-md focus:outline-none text-blue-500 bg-blue-50"><button class="transition-colors duration-200 ease-in-out focus:outline-none w-full text-left hover:underline">
-		Pagination
-	</button> <!----></div><div class="flex items-center justify-between px-4 py-2 mt-1 rounded-md focus:outline-none text-gray-500 hover:text-blue-500 transition-colors duration-200 transform"><button class="transition-colors duration-200 ease-in-out focus:outline-none w-full text-left hover:underline">
-		Footers
-	</button> <!----></div></nav></div>
+	<div class="fixed inset-y-0 left-0 z-50 flex flex-col w-64 h-screen px-4 py-8 overflow-y-auto transition duration-200 transform bg-white border-gray-200 shadow-lg xl:translate-x-0 xl:static xl:inset-0 -translate-x-full ease-in">
+		<nav class="mt-6">
+			<div class="flex items-center justify-between px-4 py-2 mt-1 rounded-md focus:outline-none text-gray-500 hover:text-blue-500 transition-colors duration-200 transform">
+				<button class="transition-colors duration-200 ease-in-out focus:outline-none w-full text-left hover:underline">
+					Alerts
+				</button> </div><div class="flex items-center justify-between px-4 py-2 mt-1 rounded-md focus:outline-none text-gray-500 hover:text-blue-500 transition-colors duration-200 transform"><button class="transition-colors duration-200 ease-in-out focus:outline-none w-full text-left hover:underline">
+				Authentication
+			</button> </div><div class="flex items-center justify-between px-4 py-2 mt-1 rounded-md focus:outline-none text-gray-500 hover:text-blue-500 transition-colors duration-200 transform"><button class="transition-colors duration-200 ease-in-out focus:outline-none w-full text-left hover:underline">
+				Breadcrumbs
+			</button> </div><div class="flex items-center justify-between px-4 py-2 mt-1 rounded-md focus:outline-none text-gray-500 hover:text-blue-500 transition-colors duration-200 transform"><button class="transition-colors duration-200 ease-in-out focus:outline-none w-full text-left hover:underline">
+				Buttons
+			</button> </div><div class="flex items-center justify-between px-4 py-2 mt-1 rounded-md focus:outline-none text-gray-500 hover:text-blue-500 transition-colors duration-200 transform"><button class="transition-colors duration-200 ease-in-out focus:outline-none w-full text-left hover:underline">
+				Cards
+			</button> </div><div class="flex items-center justify-between px-4 py-2 mt-1 rounded-md focus:outline-none text-gray-500 hover:text-blue-500 transition-colors duration-200 transform"><button class="transition-colors duration-200 ease-in-out focus:outline-none w-full text-left hover:underline">
+				CTA
+			</button> </div><div class="flex items-center justify-between px-4 py-2 mt-1 rounded-md focus:outline-none text-gray-500 hover:text-blue-500 transition-colors duration-200 transform"><button class="transition-colors duration-200 ease-in-out focus:outline-none w-full text-left hover:underline">
+				FAQ
+			</button> </div><div class="flex items-center justify-between px-4 py-2 mt-1 rounded-md focus:outline-none text-gray-500 hover:text-blue-500 transition-colors duration-200 transform"><button class="transition-colors duration-200 ease-in-out focus:outline-none w-full text-left hover:underline">
+				Features
+			</button> </div><div class="flex items-center justify-between px-4 py-2 mt-1 rounded-md focus:outline-none text-gray-500 hover:text-blue-500 transition-colors duration-200 transform"><button class="transition-colors duration-200 ease-in-out focus:outline-none w-full text-left hover:underline">
+				Forms
+			</button> </div><div class="flex items-center justify-between px-4 py-2 mt-1 rounded-md focus:outline-none text-gray-500 hover:text-blue-500 transition-colors duration-200 transform"><button class="transition-colors duration-200 ease-in-out focus:outline-none w-full text-left hover:underline">
+				Dropdowns
+			</button> </div><div class="flex items-center justify-between px-4 py-2 mt-1 rounded-md focus:outline-none text-gray-500 hover:text-blue-500 transition-colors duration-200 transform"><button class="transition-colors duration-200 ease-in-out focus:outline-none w-full text-left hover:underline">
+				Navbars
+			</button> </div><div class="flex items-center justify-between px-4 py-2 mt-1 rounded-md focus:outline-none text-gray-500 hover:text-blue-500 transition-colors duration-200 transform"><button class="transition-colors duration-200 ease-in-out focus:outline-none w-full text-left hover:underline">
+				Heros
+			</button> </div><div class="flex items-center justify-between px-4 py-2 mt-1 rounded-md focus:outline-none text-gray-500 hover:text-blue-500 transition-colors duration-200 transform"><button class="transition-colors duration-200 ease-in-out focus:outline-none w-full text-left hover:underline">
+				Sidebar
+			</button> </div><div class="flex items-center justify-between px-4 py-2 mt-1 rounded-md focus:outline-none text-gray-500 hover:text-blue-500 transition-colors duration-200 transform"><button class="transition-colors duration-200 ease-in-out focus:outline-none w-full text-left hover:underline">
+				Sections
+			</button> </div><div class="flex items-center justify-between px-4 py-2 mt-1 rounded-md focus:outline-none text-gray-500 hover:text-blue-500 transition-colors duration-200 transform"><button class="transition-colors duration-200 ease-in-out focus:outline-none w-full text-left hover:underline">
+				Tabs
+			</button> </div><div class="flex items-center justify-between px-4 py-2 mt-1 rounded-md focus:outline-none text-gray-500 hover:text-blue-500 transition-colors duration-200 transform"><button class="transition-colors duration-200 ease-in-out focus:outline-none w-full text-left hover:underline">
+				Teams
+			</button> </div><div class="flex items-center justify-between px-4 py-2 mt-1 rounded-md focus:outline-none text-gray-500 hover:text-blue-500 transition-colors duration-200 transform"><button class="transition-colors duration-200 ease-in-out focus:outline-none w-full text-left hover:underline">
+				Pricing
+			</button> </div><div class="flex items-center justify-between px-4 py-2 mt-1 rounded-md focus:outline-none text-blue-500 bg-blue-50"><button class="transition-colors duration-200 ease-in-out focus:outline-none w-full text-left hover:underline">
+				Pagination
+			</button> </div><div class="flex items-center justify-between px-4 py-2 mt-1 rounded-md focus:outline-none text-gray-500 hover:text-blue-500 transition-colors duration-200 transform"><button class="transition-colors duration-200 ease-in-out focus:outline-none w-full text-left hover:underline">
+				Footers
+			</button> </div></nav></div>
                         
 
 	<button type="button" class="fixed z-50 flex items-center justify-center p-3 text-white bg-gray-900 rounded-full focus:outline-none xl:hidden bottom-24 right-8">
 		<span class="sr-only">Open site navigation</span>
 		<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-8 h-8">
 			<path d="M21 18H9V16H21V18ZM21 13H3V11H21V13ZM21 8H9V6H21V8Z" fill="currentColor"></path>
-		</svg> <!---->
+		</svg> 
 	</button>
 </template>
 

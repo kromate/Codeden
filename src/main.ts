2a1248fcdbecd5f460a32f227ce1424acbb89a6c
kromate <akpananthony33@gmail.com>
2022-05-21 10:14:32 +0100

    feat: adding eslint

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import "@/assets/styles/tailwind.css";
-import { router as setupRouter } from "@/router";
+import { createApp } from 'vue'
+import App from './App.vue'
+import '@/assets/styles/tailwind.css'
+import { router as setupRouter } from '@/router'
 import { registerComponents } from './plugins/components/index'
 
 const init = async () => {
-  const router = await setupRouter;
-  const app = createApp(App);
-  await registerComponents(app)
-  // await router.isReady()
+	const router = await setupRouter
+	const app = createApp(App)
+	await registerComponents(app)
+	// await router.isReady()
 
-  app.use(router).mount("#app");
-};
+	app.use(router).mount('#app')
+}
 
-init().then();
+init().then()

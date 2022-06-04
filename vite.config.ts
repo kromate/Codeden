cb642e9acbf3788b83f97e32b31639ef73a79116
kromate <akpananthony33@gmail.com>
2022-06-04 15:49:56 +0100

    feat: eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,11 @@
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 import { defineConfig } from "vite";
-import eslintPlugin from "vite-plugin-eslint";
-import Pages from "vite-plugin-pages";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    eslintPlugin(),
-    vue(),
-   	Pages({
-			dirs: 'src/views',
-			routeStyle: 'nuxt',
-			extendRoute: (route: any) => {
-				const path = route.path.split('/')
-				const lastIndex = path.length - 1
-				if (path[lastIndex] && path[lastIndex].includes(':')) path[lastIndex] = path[lastIndex] + '/'
-				return { ...route, path: path.join('/') }
-			}
-		}),
-  ],
-  
+  plugins: [vue()],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
